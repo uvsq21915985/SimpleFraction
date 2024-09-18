d597aa836120246d1d971d118dac7ad5cc6e3f37
Amine <amine75135@gmail.com>
2024-09-18 16:48:36 +0200

    test

---
+++ app/Main.java
@@ -0,0 +1,9 @@
+package app;
+import util.Fraction;
+
+public class Main {
+    public static void main(String[] args) {
+        Fraction a = new Fraction();
+        System.out.println(a);
+    }
+}
---
+++ util/Fraction.java
@@ -0,0 +1,7 @@
+package util;
+
+public class Fraction {
+    public String toString(){
+        return "Je suis une Fraction.";
+    }
+}
