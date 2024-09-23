294ab63dec845d6ffecafeb9fcd017583c0adc6b
Amine <amine75135@gmail.com>
2024-09-23 11:41:51 +0200

    Q1-2-3-4

--- app/Main.java
+++ app/Main.java
@@ -1,9 +1,26 @@
 package app;
+import java.util.Scanner;
 import util.Fraction;
 
 public class Main {
-    public static void main(String[] args) {
-        Fraction a = new Fraction();
-        System.out.println(a);
+    public static void main(String[] args) throws IllegalAccessException {
+        try (Scanner scanner = new Scanner(System.in)){
+            System.out.print( "Veuillez saisir un numerateur : " );
+            int a = scanner.nextInt();
+
+            System.out.print( "Veuillez saisir un denominateur : " );
+            int b = scanner.nextInt();
+            
+            Fraction f1 = new Fraction(a, b);
+            System.out.printf("La fraction est " + f1 + "\n");
+
+            Fraction f2 = new Fraction(0, 1);
+            System.out.printf("La fraction est " + f2 + "\n");
+
+            Fraction f3 = new Fraction();
+            System.out.printf("La fraction est " + f3 + "\n");
+
+            System.out.printf("Voici ZERO : " + Fraction.ZERO + " et UN : " + Fraction.UN + "\n");
+        }
     }
 }
--- util/Fraction.java
+++ util/Fraction.java
@@ -1,7 +1,35 @@
 package util;
 
 public class Fraction {
+    private int num;
+    private int den;
+    public static final Fraction ZERO = new Fraction(0, 1);
+    public static final Fraction UN = new Fraction(1, 1);
+
+    public Fraction() { this.num = 0; this.den = 1;}
+    public Fraction(int num, int den) { this.num = num; this.den = den;}
+
+    public int getNum(){
+        return num;
+    }
+
+    public int getDen(){
+        return den;
+    }
+
+
+    public void setNum(int newNum){
+        num = newNum;
+    }
+
+    public void setDen(int newDen) throws IllegalAccessException{
+        if (newDen == 0) {
+            throw new IllegalAccessException("Division par 0.");  
+        }
+        den = newDen;
+    }
+
     public String toString(){
-        return "Je suis une Fraction.";
+        return this.getNum() + "/" + this.getDen();
     }
 }
